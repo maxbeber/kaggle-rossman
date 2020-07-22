962e480e2dcb3bda789954d48b8e629124d2b02e
maxbeber <max.beber@gmail.com>
2020-07-22 15:05:22 +0200

    Update in the feature engineering functions.

--- model.py
+++ model.py
@@ -1,26 +1,30 @@
+import datetime
 import numpy as np
 import sys
 import pandas as pd
+import pickle
 import xgboost as xgb
-from joblib import dump
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-features = ["Store", "DayOfWeek", "Customers", "Open", "Promo", \
-	"CompetitionDistance", "CompetitionOpenSinceMonth", \
-	"CompetitionOpenSinceYear", "Promo2", "Promo2SinceWeek", \
-	"Promo2SinceYear", "StateHoliday_0", "StateHoliday_a", \
+features = [ \
+	"Store", "DayOfWeek", "Customers", "Open", "Promo", \
+	"CompetitionDistance","Comp_since_days", \
+	"Promo2", 'Promo_since_days', \
+	"StateHoliday_0", "StateHoliday_a", \
 	"StateHoliday_b", "StateHoliday_c", "StoreType_a", "StoreType_b", \
 	"StoreType_c", "StoreType_d", "Assortment_a", "Assortment_b", \
 	"Assortment_c", "PromoInterval_Feb,May,Aug,Nov", \
 	"PromoInterval_Jan,Apr,Jul,Oct", "PromoInterval_Mar,Jun,Sept,Dec", \
-	"Year", "Month", "Day", "WeekOfYear"]
+	"Year", "Month", "WeekOfYear","MedianSalesByStore", "MedianSalesByDayOfWeek", \
+	"Monetary" , "Recency", "Customer_avg"]
 params = {
 	"objective": "reg:squarederror",
 	"max_depth": 5,
-    "nthread": 4
+	"nthread": 4
 }
-num_boost_round = 1300
+num_boost_round = 130
+test_features_eng = {}
 
 
 def rmspe(preds, actuals):
@@ -50,16 +54,66 @@ def data_cleaning(df):
 	return df
 
 
-def feature_engineering(df):
-    df["Year"] = df.Date.dt.year
-    df["Month"] = df.Date.dt.month
-    df["Day"] = df.Date.dt.day
-    df["WeekOfYear"] = df.Date.dt.weekofyear
-    average_sales_by_store = df.groupby("Store")["Sales"].median()
-    df["AverageSalesByStore"] = df.Store.map(average_sales_by_store)
-    average_sales_by_day_of_week = df.groupby("DayOfWeek")["Sales"].median()
-    df["AverageSalesByDayOfWeek"] = df.DayOfWeek.map(average_sales_by_store)
-    return df
+def time_distance(df,sinceWorM,sinceYr,distance_name,week=None):
+	# sinceWorM : string of column name with ...sinceMonth, if sinceWeek => week=1
+	# sinceYr : string of column name with ... SinceYear
+	# distance_name : ex. competition_dist or promo_dist
+	m_999 = df[sinceWorM] != -999
+	y_999 = df[sinceYr] != -999
+	glob_999 = m_999 + y_999
+	df_mask = df.loc[glob_999,:]
+	if week == 1:
+		df_mask.loc[:,sinceWorM]=df_mask.loc[:,sinceWorM]//4
+		df_mask.loc[:,sinceWorM] = df_mask.loc[:,sinceWorM].apply(str).replace({"0":"1","13":"12"})
+	df_mask.loc[:,"temp_MY"] = pd.to_datetime(df_mask.loc[:,sinceYr].apply(str) + "-" + df_mask.loc[:,sinceWorM].apply(str) + "-01")
+	df_mask.loc[:,distance_name] = (df_mask.loc[:,"Date"] - df_mask.loc[:,"temp_MY"]).dt.days
+	# concat back columns with distance in days to original df
+	new_df = pd.concat([df,df_mask.loc[:,distance_name]],axis=1, sort=False)
+	new_df.loc[:,distance_name] = new_df.loc[:,distance_name].fillna(value=-9999).astype("int",copy=False)
+	return new_df
+
+
+def feature_engineering(df, test_set=None):
+	df["Year"] = df.Date.dt.year
+	df["Month"] = df.Date.dt.month
+	df["Day"] = df.Date.dt.day
+	df["WeekOfYear"] = df.Date.dt.weekofyear
+	df = time_distance(df, 'CompetitionOpenSinceMonth', 'CompetitionOpenSinceYear', 'Comp_since_days')
+	df = time_distance(df, 'Promo2SinceWeek', 'Promo2SinceYear','Promo_since_days', 1)
+	if test_set:
+		# Searching values back in dictionnary (computed on the TRAINING SET)
+		df["MedianSalesByStore"] = df.Store.map(test_features_eng["MedianSalesByStore"])
+		df["MedianSalesByDayOfWeek"] = df.DayOfWeek.map(test_features_eng["MedianSalesByDayOfWeek"])
+		df["Monetary"] = df.Store.map(test_features_eng["Monetary"])
+		df["Recency"] = df.Store.map(test_features_eng["Recency"])
+		df["Customer_avg"] = df.Store.map(test_features_eng["Customer_avg"])
+	else:
+		median_sales_by_store = df.groupby("Store")["Sales"].median()
+		# Convert to dictionnaire to use in TEST SET and Save it in global Dictionnary
+		test_features_eng["MedianSalesByStore"] = median_sales_by_store.to_dict()
+		df["MedianSalesByStore"] = df.Store.map(median_sales_by_store)
+		median_sales_by_day_of_week = df.groupby("DayOfWeek")["Sales"].median()
+		# Convert to dictionnaire to use in TEST SET and Save it in global Dictionnary
+		test_features_eng["MedianSalesByDayOfWeek"] = median_sales_by_day_of_week.to_dict()
+		df["MedianSalesByDayOfWeek"] = df.DayOfWeek.map(median_sales_by_store)
+		## RFM
+		today = datetime.datetime(2014,8,1)
+		agg_rule = {'Date': lambda x: (today - x.max()).days, 'Customers': lambda x: x.median(), 'Sales': lambda x: x.sum()}
+		rfm_score = df.groupby('Store').agg(agg_rule)
+		rfm_score.columns = ["R", "C", "M"]
+		quantiles = rfm_score.quantile(q=[0.25, 0.5, 0.75])
+		## Recency Feature
+		test_features_eng["Recency"] = rfm_score["R"].to_dict()
+		df["Recency"] = df.Store.map(test_features_eng["Recency"])
+		# Convert to dictionnaire to use in TEST SET and Save it in global Dictionnary
+		## Monetary Feature
+		rfm_score["M_segments"] = rfm_score['M'].apply(m_group, args=('M',quantiles))
+		# Convert to dictionnaire to use in TEST SET and Save it in global Dictionnary
+		test_features_eng["Monetary"] = rfm_score["M_segments"].to_dict()
+		df["Monetary"] = df.Store.map(test_features_eng["Monetary"])
+		test_features_eng["Customer_avg"] = rfm_score["C"].to_dict()
+		df["Customer_avg"] = df.Store.map(test_features_eng["Customer_avg"])
+	return df
 
 
 def load_data():
@@ -75,6 +129,17 @@ def load_data():
 	print("Join train and store")
 	train = pd.merge(train, store, on="Store")
 	return train
+
+
+def m_group(x, type, quantiles):
+	if x <= quantiles[type].iloc[0]:
+		return 4
+	elif x <= quantiles[type].iloc[1]:
+		return 3
+	elif x <= quantiles[type].iloc[2]:
+		return 2
+	else:
+		return 1
 
 
 def display_features_by_importance(features):
@@ -100,6 +165,7 @@ def display_metrics(x_train, y_train, x_holdout, y_holdout):
 	print("#################################################")
 	print("")
 
+
 print("Loading training data.")
 data = load_data()
 
@@ -117,7 +183,8 @@ print("Train Random Forrest")
 dtrain = xgb.DMatrix(X_train[features], Y_train)
 dvalid = xgb.DMatrix(X_holdout[features], Y_holdout)
 watchlist = [(dtrain, "train"), (dvalid, "eval")]
-gbm = xgb.train(params, dtrain, num_boost_round, evals=watchlist, early_stopping_rounds=50, verbose_eval=True)
+gbm = xgb.XGBRegressor()
+gbm = xgb.train(params, dtrain, num_boost_round, evals=watchlist, early_stopping_rounds=25, verbose_eval=True)
 
 print("Computing predictions")
 yhat_holdout = gbm.predict(xgb.DMatrix(X_holdout[features]))
@@ -131,7 +198,6 @@ display_features_by_importance(feature_score)
 
 # save model to file
 print("Save the model to rossman.dat")
-dump(gbm, "rossman.dat")
-
+pickle.dump(gbm, open("rossman.dat", "wb"))
 ## Measure the error
 display_metrics(X_train, Y_train, X_holdout, Y_holdout)
